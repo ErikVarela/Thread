6dcc44e42fd9cc923129ae207d823bd86f3f2fcf
Erik Varela <119821019+ErikVarela@users.noreply.github.com>
2025-05-27 20:47:37 -0300

    Update Carro.java

--- src/Carro.java
+++ src/Carro.java
@@ -1,50 +1,51 @@
-public class Carro extends Thread{
-    private BombaCombustivel    bombaCombustivel;
-    private String              modelo;
-    private int                 idCar;
-    private int                 tanqueCombustivel;
-    
-    public Carro (String m, int id, BombaCombustivel bombaCombustivel){
-    this.modelo             = m;
-    this.idCar              = id;
-    this.tanqueCombustivel  = 40;
-    this.bombaCombustivel   = bombaCombustivel;
+public class Carro extends Thread {
+    private BombaCombustivel bombaCombustivel;
+    private String modelo;
+    private int idCar;
+    private int tanqueCombustivel;
+
+    public Carro(String modelo, int id, BombaCombustivel bombaCombustivel) {
+        this.modelo = modelo;
+        this.idCar = id;
+        this.tanqueCombustivel = 40;
+        this.bombaCombustivel = bombaCombustivel;
     }
 
     @Override
-    public void run(){
+    public void run() {
         System.out.println(this.modelo + " COMEÇOU A RODAR!");
-        
-        while(getTanqueCombustivel() > 0){
-           try{
-               Thread.sleep(500);
-               System.out.println(this.modelo
-                                    + " - " 
-                                    + this.idCar
-                                    + " - " 
-                                    + this.getTanqueCombustivel()
-                                    + " l");
-                setTanqueCombustivel(getTanqueCombustivel() - 1);  
-                if(this.tanqueCombustivel <= 10){
-                    System.out.println(this.modelo + "PRECISA ABASTECER!");
+
+        while (getTanqueCombustivel() > 0) {
+            try {
+                Thread.sleep(500);
+
+                System.out.println(this.modelo
+                        + " - " + this.idCar
+                        + " - " + getTanqueCombustivel() + " l");
+
+                setTanqueCombustivel(getTanqueCombustivel() - 1);
+
+                if (this.tanqueCombustivel <= 10) {
+                    System.out.println(this.modelo + " PRECISA ABASTECER!");
                     bombaCombustivel.abastecer(this);
                 }
-           } catch (InterruptedException ex){
-                
-           }
-    }
-        System.out.println(this.modelo + "FICOU SEM COMBUSTÍVEL!");
-    
-}
-    public void setTanqueCombustivel(int qtde){
-        this.tanqueCombustivel = qtde; 
-   }
+            } catch (InterruptedException ex) {
+                System.out.println("Erro na thread do carro: " + ex.getMessage());
+            }
+        }
 
-    /**
-     * @return the tanqueCombustivel
-     */
+        System.out.println(this.modelo + " FICOU SEM COMBUSTÍVEL!");
+    }
+
+    public void setTanqueCombustivel(int qtde) {
+        this.tanqueCombustivel = qtde;
+    }
+
     public int getTanqueCombustivel() {
         return tanqueCombustivel;
     }
-    
+
+    public String getModelo() {
+        return this.modelo;
+    }
 }
